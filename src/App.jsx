9d973d827d67ef777853c6059b8341377dfac212
mdsamiulislam54 <samiulm5332@gmail.com>
2025-04-15 21:59:29 +0600

    Navbar section design update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 
 import './App.css'
-
+import Navbar from './Componants/Navbar/Navbar'
+import { BrowserRouter as Router, Route  } from 'react-router-dom'
 
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
 
   return (
     <>
-      
+    <Router>
+    <Navbar/>
+    </Router>
+    
       
     </>
   )
