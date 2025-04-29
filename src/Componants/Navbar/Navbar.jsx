2918c17f6713deb88c2a86b9d521e0a8e49c88c3
mdsamiulislam54 <samiulm5332@gmail.com>
2025-04-29 10:52:19 +0600

    create account user add

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { RiAccountPinBoxFill } from "react-icons/ri";
 
 import i18n from "../../i18n";
 import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import auth from "../../firbase-init";
+import Swal from 'sweetalert2'
+
 const Navbar = () => {
-  const [isForm, setIsFrom] = useState(false);
+  const [showFormType, setShowFormType] = useState(null);
+
   const [theme, setTheme] = useState("");
   const navigate = useNavigate();
+  const fromRef = useRef(null)
   useEffect(() => {
     const savedThems = localStorage.getItem("theme") || "light";
     setTheme(savedThems);
@@ -22,13 +31,54 @@ const Navbar = () => {
 
   const { t } = useTranslation();
 
-  const handleServicesPage = (Sname) => {
-    navigate(`/services/${Sname}`);
+  const handleEmailandPasswordBtn = () => {
+    setShowFormType("signup");
   };
 
-  const handleEmailandPasswordBtn = ()=>{
-    setIsFrom(true)
-  }
+  const handleSignInBtn = () => {
+    setShowFormType("signin");
+  };
+
+  const handleBack = () => {
+    setShowFormType(null);
+  };
+
+  const handleCreateAccount = (e) => {
+    e.preventDefault();
+    const fromFild = fromRef.current
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name,email,password)
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        updateProfile(auth.currentUser, {
+          displayName: name
+        })
+        document.getElementById("my_modal_1").close();
+        Swal.fire({
+          icon: 'success',
+          title: 'Registration Successful!',
+          text: `Welcome, ${name}!`,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'Okay'
+        })
+        fromFild.reset()
+        console.log(user);
+      })
+      .catch((error) => {
+        document.getElementById("my_modal_1").close();
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.message,
+          confirmButtonColor: '#d33',
+        })
+      });
+      
+  };
 
   return (
     <nav className="navbar bg-base-100 shadow-md sticky top-0 z-100">
@@ -205,89 +255,156 @@ const Navbar = () => {
 
         <dialog id="my_modal_1" className="modal">
           <div className="modal-box">
-          <div className= {`flex flex-col space-y-5  ${isForm ? "hidden":"block"}`}>
-          <button onClick={handleEmailandPasswordBtn} className={`btn bg-white text-black border-[#e5e5e5]`}>
-              <svg
-                aria-label="Email icon"
-                width="16"
-                height="16"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+            <div className={`flex flex-col space-y-5 ${showFormType  === null ? "block":"hidden"} `}>
+              <button
+                onClick={() => {
+                  handleSignInBtn("signin");
+                }}
+                className={`btn bg-white text-black border-[#e5e5e5]`}
               >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="black"
+                <svg
+                  aria-label="Email icon"
+                  width="16"
+                  height="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
                 >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              Login with Email
-            </button>
-            <button className="btn bg-white text-black border-[#e5e5e5]">
-              <svg
-                aria-label="Google logo"
-                width="16"
-                height="16"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    fill="none"
+                    stroke="black"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                Login with Email
+              </button>
+              <button className="btn bg-white text-black border-[#e5e5e5]">
+                <svg
+                  aria-label="Google logo"
+                  width="16"
+                  height="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <g>
+                    <path d="m0 0H512V512H0" fill="#fff"></path>
+                    <path
+                      fill="#34a853"
+                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                    ></path>
+                    <path
+                      fill="#4285f4"
+                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                    ></path>
+                    <path
+                      fill="#fbbc02"
+                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                    ></path>
+                    <path
+                      fill="#ea4335"
+                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                    ></path>
+                  </g>
+                </svg>
+                Login with Google
+              </button>
+              <span className="text-center text-sm text-red-500">
+                Don't have an account? Please create an account.
+              </span>
+
+              <button
+                onClick={() => handleEmailandPasswordBtn("signup")}
+                className={`btn bg-white text-black border-[#e5e5e5]`}
               >
-                <g>
-                  <path d="m0 0H512V512H0" fill="#fff"></path>
-                  <path
-                    fill="#34a853"
-                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                  ></path>
-                  <path
-                    fill="#4285f4"
-                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                  ></path>
-                  <path
-                    fill="#fbbc02"
-                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                  ></path>
-                  <path
-                    fill="#ea4335"
-                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                  ></path>
-                </g>
-              </svg>
-              Login with Google
-            </button>
-          </div>
-            <form action="" className={`${isForm ? "block":"hidden"}`}>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
-                <legend className="fieldset-legend text-center text-xl">
-                  Sing In
-                </legend>
+                <RiAccountPinBoxFill />
+                Create a account
+              </button>
+            </div>
+            {showFormType === "signin" && (
+              <form ref={fromRef}  className={``}>
+                <span className="btn" onClick={() => handleBack(null)}>
+                  <IoIosArrowRoundBack size={24} />
+                </span>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                  <legend className="fieldset-legend text-center text-xl">
+                    Sing In
+                  </legend>
 
-                <label className="label">Email:</label>
-                <input
-                  type="text"
-                  className="input w-full"
-                  placeholder="Enter Your Email.."
-                />
+                  <label className="label">Email:</label>
+                  <input
+                    type="text"
+                    name="email"
+                    className="input w-full"
+                    placeholder="Enter Your Email.."
+                  />
 
-                <label className="label">Password:</label>
-                <input
-                  type="password"
-                  className="input w-full"
-                  placeholder="Enter Your Password.."
-                />
+                  <label className="label">Password:</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="input w-full"
+                    placeholder="Enter Your Password.."
+                  />
 
-                <input
-                  type="submit"
-                  className="btn text-white bg-red-500 my-4"
-                  placeholder="Name"
-                />
-                <div>
-                  <p className="text-xl font-bold text-center">Or</p>
-                </div>
-              </fieldset>
-            </form>
+                  <input
+                    type="submit"
+                    className="btn text-white bg-red-500 my-4"
+                    placeholder="Name"
+                    value="Sign In"
+                  />
+                </fieldset>
+              </form>
+            )}
+            {showFormType === "signup" && (
+              <form
+              ref={fromRef}
+               onSubmit={handleCreateAccount}
+                className={``}
+              >
+                <span className="btn" onClick={() => handleBack(false)}>
+                  <IoIosArrowRoundBack size={24} />
+                </span>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                  <legend className="fieldset-legend text-center text-xl">
+                    Sing Up
+                  </legend>
+                  
+                  <label className="label">Name:</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="input w-full"
+                    placeholder="Enter Your Name.."
+                  />
+                  <label className="label">Email:</label>
+                  <input
+                    type="text"
+                    name="email"
+                    className="input w-full"
+                    placeholder="Enter Your Email.."
+                  />
+
+                  <label className="label">Password:</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="input w-full"
+                    placeholder="Enter Your Password.."
+                  />
+
+                  <input
+                    type="submit"
+                    className="btn text-white bg-red-500 my-4"
+                    placeholder="Name"
+                    value="Sign Up"
+                  />
+                </fieldset>
+              </form>
+            )}
 
             <div className="modal-action">
               <form method="dialog">
