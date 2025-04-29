019610c2c293dbc22c021c74a6185d95128de8b3
mdsamiulislam54 <samiulm5332@gmail.com>
2025-04-29 08:49:14 +0600

    uer authentication update

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import { useTranslation } from "react-i18next";
 
 import i18n from "../../i18n";
 import { Link, useNavigate } from "react-router-dom";
-
 const Navbar = () => {
+  const [isForm, setIsFrom] = useState(false);
   const [theme, setTheme] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
@@ -25,6 +25,10 @@ const Navbar = () => {
   const handleServicesPage = (Sname) => {
     navigate(`/services/${Sname}`);
   };
+
+  const handleEmailandPasswordBtn = ()=>{
+    setIsFrom(true)
+  }
 
   return (
     <nav className="navbar bg-base-100 shadow-md sticky top-0 z-100">
@@ -101,7 +105,7 @@ const Navbar = () => {
                     <li onClick={() => handleServicesPage("Wiring")}>
                       <Link>{t("services.service3")}</Link>
                     </li>
-   
+
                     <li onClick={() => handleServicesPage("ShortCiruit")}>
                       <Link>{t("services.service4")}</Link>
                     </li>
@@ -189,10 +193,112 @@ const Navbar = () => {
           </div>
 
           {/* Login Button */}
-          <a className="btn bg-red-600 px-5 text-white py-2 text-sm font-semibold">
+          <a
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+            className="btn bg-red-600 px-5 text-white py-2 text-sm font-semibold"
+          >
             {t("login.name")}
           </a>
         </div>
+        {/* modal  */}
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+          <div className= {`flex flex-col space-y-5  ${isForm ? "hidden":"block"}`}>
+          <button onClick={handleEmailandPasswordBtn} className={`btn bg-white text-black border-[#e5e5e5]`}>
+              <svg
+                aria-label="Email icon"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="black"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              Login with Email
+            </button>
+            <button className="btn bg-white text-black border-[#e5e5e5]">
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
+          </div>
+            <form action="" className={`${isForm ? "block":"hidden"}`}>
+              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                <legend className="fieldset-legend text-center text-xl">
+                  Sing In
+                </legend>
+
+                <label className="label">Email:</label>
+                <input
+                  type="text"
+                  className="input w-full"
+                  placeholder="Enter Your Email.."
+                />
+
+                <label className="label">Password:</label>
+                <input
+                  type="password"
+                  className="input w-full"
+                  placeholder="Enter Your Password.."
+                />
+
+                <input
+                  type="submit"
+                  className="btn text-white bg-red-500 my-4"
+                  placeholder="Name"
+                />
+                <div>
+                  <p className="text-xl font-bold text-center">Or</p>
+                </div>
+              </fieldset>
+            </form>
+
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn hover:bg-red-500  transition-all duration-500">
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
       </div>
     </nav>
   );
